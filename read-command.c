664a6d6212789c9023f1fc8381f1d9dc82615457
Minjian Wang <minjian@lnxsrv01.seas.ucla.edu>
2013-04-04 00:11:19 -0700

    input

--- read-command.c
+++ read-command.c
@@ -2,8 +2,9 @@
 
 #include "command.h"
 #include "command-internals.h"
-
+#include "alloc.h"
 #include <error.h>
+#include <stdio.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
@@ -38,12 +39,45 @@ command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
-  //Let's just try to read the input in first.
+  //=========Let's just try to read the input in first.============//
+  
+  size_t bufferSize = 1024;
+  size_t bufferIterator = 0;
+  char value;
+  char* buffer = (char*) checked_malloc(bufferSize);
+
+  while ((value = get_next_byte(get_next_byte_argument)) != EOF) //store into value while it isn't EOF
+  {
+    buffer[bufferIterator++] = value;
+    //    printf("looped once: %c \n", value);
+    //    printf("bufferSize: %d \n", (int) bufferSize);
+    if (bufferSize == bufferIterator){
+      buffer =  (char*) checked_grow_alloc(buffer, &bufferSize);
+      //printf("called checked_realloc: %d\n", (int) bufferSize);
+    }
+  }
+  if (bufferSize == bufferIterator){
+    buffer =  (char*) checked_grow_alloc(buffer, &bufferSize);
+    //printf("called checked_realloc: %d\n", (int) bufferSize);
+  }
+  buffer[bufferIterator] = '\0';
+
+  //printf("bufferIterator: %d \n", (int) bufferIterator);
+  //puts(buffer);  //will output to stdout
+  
+  //=========Let's tokenize the stream============//
+  //we need to do the same thing as before, except instead of taking from the filestream and putting it into the array.
+  //Now take the array and put it into a token stream.
+
   
   
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
+  
+  //=========Let's return a command stream============//
+  command_stream_t fake = (command_stream_t) checked_malloc(sizeof(command_stream_t));
+  fake->size = 1;
+  fake->iterator = 0;
+  return fake;
+  
   
 
   //error (1, 0, "command reading not yet implemented");
@@ -53,6 +87,8 @@ make_command_stream (int (*get_next_byte) (void *),
 command_t
 read_command_stream (command_stream_t s)
 {
+  s->size = 1;
+  
   /* FIXME: Replace this with your implementation too.  */
   error (1, 0, "command reading not yet implemented");
   return 0;
