8bb2c0e8c0be2d801a10fd87fd7393ea369dc090
Minjian Wang <minjian@lnxsrv01.seas.ucla.edu>
2013-04-05 22:44:47 -0700

    tokenization work 1/2done

--- read-command.c
+++ read-command.c
@@ -30,6 +30,7 @@ typedef enum {
   MISC_TOKEN
 } token_type;
 
+
 typedef struct token{
   token_type type;
   char* words;
@@ -40,6 +41,19 @@ typedef struct token_stream{
  struct token_stream* next;
  struct token_stream* prev;
 } token_stream;
+
+/*
+struct token{
+  token_type type;
+  char* words;
+};
+
+struct token_stream{
+ token m_token;
+ struct token_stream* next;
+ struct token_stream* prev;
+};
+*/
 
 //could never get this piece of shit working
 /*token_stream_t insert_token (token_stream_t root, token_t target_token){
@@ -88,9 +102,116 @@ make_command_stream (int (*get_next_byte) (void *), void *get_next_byte_argument
   //=========Let's tokenize the stream============//
   //we need to do the same thing as before, except instead of taking from the filestream and putting it into the array.
   //Now take the array and put it into a token stream.
-
+  //Could never get the outside function working due to all that referential bullshit.  Let's just do it inline.
 
   
+  token_stream_t tstream = (token_stream_t) checked_malloc(sizeof(token_stream_t));
+  int bufferIteratorT = 0;
+  int comment = 0;
+  
+  token_type type;
+  
+  //do a dual parsing here where we handle two characters at the same time. This makes the && and || and comments easy
+  //Need to check if the buffer is completely empty (has '\0' as the only character).  Then we don't do anything so we  //can just return
+
+  
+
+  if (buffer[bufferIteratorT] == '\0')
+  {  
+    command_stream_t empty = (command_stream_t) checked_malloc(sizeof(command_stream_t));
+    empty->size = 0;
+    return empty;
+  }
+  char first;
+  char second;
+  while (buffer[bufferIteratorT+1] != '\0')
+  {
+    first = buffer[bufferIteratorT];
+    second = buffer[bufferIteratorT+1];
+
+    //Check for &&
+    if (first == '&')
+      {
+        if (second == '&')
+          { 
+     	    type = AND_TOKEN;
+	    bufferIteratorT++;//deals with the fact that && is two chars
+          }
+        else
+          {
+	    type = MISC_TOKEN;  //We can test for MISC_TOKEN later, this is an invalid input, only 1 &
+          }
+	  //TODO:  insert token here
+      }
+    
+    //Check for ||
+    else if (first == '|')
+      {
+	if (second == '|')
+	  {
+	    type = OR_TOKEN;
+	    bufferIteratorT++;
+          }
+	else
+	  {
+	    type = PIPE_TOKEN;	    
+	  }
+	//TODO:  insert token here
+      }
+
+    else if (first == ';')
+      {
+	type = SEMICOLON_TOKEN;
+	//TODO:  insert token here
+      }
+
+    else if (first == '(')
+      {
+	type = LEFT_PAREN_TOKEN;
+      }
+
+    else if (first == ')')
+      {
+	type = RIGHT_PAREN_TOKEN;
+      }
+
+    else if (first == '<')
+      {
+	type = LESS_TOKEN;
+      }
+
+    else if (first == '>')
+      {
+	type = GREATER_TOKEN;
+      }
+
+    else if (first == '\n')
+      {
+	type = NEWLINE_TOKEN;
+      }
+
+    else if ()
+      {
+
+      }
+
+    else //unknown character 
+      {
+	type = MISC_TOKEN;
+      }
+
+    //token insertion here.
+    if (first == ' ' || first == '\t')
+      {
+	//Don't insert
+      }
+    else 
+      {
+	//insert token here
+      }
+
+    bufferIteratorT++;
+  }
   
   //=========Let's return a command stream============//
   command_stream_t fake = (command_stream_t) checked_malloc(sizeof(command_stream_t));
